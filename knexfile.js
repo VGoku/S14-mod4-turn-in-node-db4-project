fde236d0e128645382e56b6f683cca01e0cedf45
David S. <neojohn2312@gmail.com>
2024-11-15 14:36:44 -0600

    knex-file-so-far

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+const shardConfig = {
+    client: "sqlite3",
+    migrations: { directory: "./data/migrations" },
+    seed: { directory: "./data/seeds" },
+    //sqlite3 below
+    useNullAsDefault: true,
+    pool: { afterCreate:(conn, done) => conn.run("PRAGMA foreign_key = ON", done) },
+}
+
+module.exports = {
+    development: {
+        ...shardConfig,
+        connection: { filename: "./data/cook_book.db3" },
+    },
+    testing: {
+        ...shardConfig,
+        connection: { filename: "./data/cook_book.test.db3" },
+    },
+    production: {}
+}
