565edb25db236b5b6b95c69f80fbd2c1a3103c7a
David S. <neojohn2312@gmail.com>
2024-11-16 09:27:38 -0600

    mig-2nd-code-passing

--- data/migrations/20241115205026_initial-migration.js
+++ data/migrations/20241115205026_initial-migration.js
@@ -4,17 +4,20 @@
  */
 exports.up = async function(knex) {
     await knex.schema
-  .createTable("recipes", table => {
-    table.increments()
+  .createTable("recipes",table => {
+   table.increments("recipe_id")
+   table.string("recipe_name", 200).notNullable().unique()
   })
-  .createTable("ingredients", table => {
-    table.increments()
+  .createTable("ingredients",table => {
+   table.increments("ingredient_id")
+   table.string("ingredient_name", 200).notNullable().unique()
+   table.string("ingredient_unit", 50)
   })
-  .createTable("steps", table => {
-    table.increments()
+  .createTable("steps",table => {
+   table.increments()
   })
-  .createTable("step_ingredients", table => {
-    table.increments()
+  .createTable("step_ingredients",table => {
+   table.increments()
   })
 }; //38:27
 
