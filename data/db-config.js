243e35dcd9174e56004b3df7f999303e6e424b17
David S. <neojohn2312@gmail.com>
2024-11-15 18:32:17 -0600

    migworking

--- data/db-config.js
+++ data/db-config.js
@@ -1,3 +1,5 @@
 const knex = require("knex");
 const configurations = require("../knexfile")
 const environment = process.env.MODE_ENV //|| "development"
+
+module.exports = knex(configurations[environment])
